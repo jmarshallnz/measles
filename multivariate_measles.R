e89a86aef407aeebb7efabf72cded90cc17eeb36
David Hayman <davidtshayman@gmail.com>
2014-06-30 23:43:37 +1200

    anova analyses of case data
    Not sure if this is correct. Seems logical to me, though the data
    manipulation is clunky. Categorical explanatory variables with
    interactions. Interestingly interactions seem unimportant.

--- multivariate_measles.R
+++ multivariate_measles.R
@@ -1,7 +1,7 @@
 ## regression analyses - measles
 ## get data
 # set wd
-setwd("~/Massey_2014/DHayman_20140627")
+setwd("~/Cambridge/Massey 2014/DHayman_20140627")
 # read data
 data<-read.csv("DHayman_20140627.csv",header=T)
 names(data)
@@ -27,7 +27,6 @@ dim(test)
 test$AgeInYears<-findInterval(test$AgeInYears,c(1,4,9,14,19,29,39,49,59,69,79))
 test$AgeInYears<-as.factor(test$AgeInYears)
 tage<-revalue(test$AgeInYears, c("0"="<1", "1"="1-3","2"="4-8","3"="9-13","4"="14-18","5"="19-28","6"="29-38","7"="39-48","8"="49-58","9"="59-68"));
-tage
 test$AgeInYears<-tage
 summary(test$EthnicityPrioritised)
 teth<-revalue(test$EthnicityPrioritised, c("European or Other"="European", "Middle Eastern/Latin American/African"="MLA",
@@ -35,3 +34,61 @@ teth<-revalue(test$EthnicityPrioritised, c("European or Other"="European", "Midd
 
 test$EthnicityPrioritised<-teth
 head(test)
+
+# want to set NZ Deprivation to the appropriate year
+## why does the following add 1 to all the values?
+testt <- within(test, NZDep<- ifelse (test$RptYear == "2007",test$NZDep06,
+                              ifelse (test$RptYear == "2008",test$NZDep06,
+                              ifelse (test$RptYear == "2009",test$NZDep06,
+                              ifelse (test$RptYear == "2010",test$NZDep06,
+                              ifelse (test$RptYear == "2011",test$NZDep06,
+                              ifelse (test$RptYear == "2012",test$NZDep06,
+                              ifelse (test$RptYear == "2013",test$NZDep13,NZDep13))))))))
+head(testt)
+tail(testt)
+str(testt)
+testt$NZDep<-as.factor(testt$NZDep-1)
+
+testtable<-aggregate( cbind( DiseaseName + as.numeric(SurvWeek)) ~ NZDep+AgeInYears+EthnicityPrioritised + RptYear, 
+                 data = testt , FUN=sum)
+dim(testtable)
+colnames(testtable)<-c("NZDep","Age","Ethnicity","Year","Cases")
+pairs(testtable,panel=panel.smooth)
+## appears very little correlation/relationships between them
+#
+# don't think the next pair of models are valid - 
+# because no year (proxy for outbreak) effect accounted for
+#
+# model1<-aov(Cases~NZDep*Age*Ethnicity,data=testtable)
+# summary(model1)
+# summary.lm(model1)
+#
+# model2<-aov(Cases~NZDep+Age+Ethnicity,data=testtable)
+# summary(model2)
+# summary.lm(model2)
+#
+# also suggests too many age categories
+str(testtable)
+levels(testtable$Age)
+levels(testtable$Age)[c(5:10)]<-"Old"
+
+model3<-aov(Cases~NZDep*Age*Ethnicity,data=testtable)
+summary(model3)
+summary.lm(model3)
+# looks like no need to keep interaction terms
+
+## account for random year effect (proxy for outbreak)
+model4<-aov(Cases~NZDep*Age*Ethnicity+Error(Year),data=testtable)
+summary(model4)
+# still looks like no need for interaction terms
+
+model5<-aov(Cases~NZDep+Age+Ethnicity+Error(Year),data=testtable)
+summary(model5)
+
+model6<-aov(Cases~NZDep+Age+Ethnicity,data=testtable)
+summary(model6)
+summary.lm(model6)
+
+## results - though many age categries, richer more likely, 1-3 & 9-13 Yrs, Europeans 
+## caveats - temporal and spatial autocorrelation, though year random effect
+## caveats - no "per capita" analyses - just raw data
