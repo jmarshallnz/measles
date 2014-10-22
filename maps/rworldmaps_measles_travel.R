e563fa8ff2cc2158653872af7e2d641da1ab8c96
dtsh2 <davidtshayman@gmail.com>
2014-10-22 18:33:55 +1300

    revised maps and pdfs for NF

--- maps/rworldmaps_measles_travel.R
+++ maps/rworldmaps_measles_travel.R
@@ -573,10 +573,98 @@ grid.arrange(totnp4, totnp1, totnp2, totnp3, ncol=2, main="Measles (2012)")
 
 ##
 
+pdf(paste("risk_measles_base.pdf"))
 grid.arrange(np2,np3, ncol=1, main="Measles incidence and vaccination cover (2012)")
+dev.off()
 
+pdf(paste("risk_measles_foreign.pdf"))
 grid.arrange(np1, np4, ncol=1, main="Foreign travellers to New Zealand and risk (Travellers * Country incidence)")
+dev.off()
 
+pdf(paste("risk_measles_nzers.pdf"))
 grid.arrange(nznp1, nznp4, ncol=1, main="New Zealand travellers and risk (Travellers * Country incidence)")
+dev.off()
 
+pdf(paste("risk_measles_tot.pdf"))
 grid.arrange(totnp1, totnp4, main="Total travellers and risk (Travellers * Country incidence)")
+dev.off()
+
+
+##
+ttimes<-read.csv("times.csv",header=T)
+lmres<-lm(res~time,data=ttimes)
+summary(lmres)
+plot(ttimes$res,xaxt="n",type="l",xlab="Date",ylab="")
+axis(1, at=1:71, labels=ttimes$mydate) 
+abline(lmres,col="red",
+       lty=2)
+acf(ttimes$res,lag.max = length(ttimes$res),plot=T,main="")
+change<-read.csv("change.csv",header=T)
+
+plot(change[18:25,1],change[18:25,4],type="l",xlab="Year",ylab="",ylim=c(0,max(change[,4])),
+     bty="n")
+ylim <- range(c(0,max(change[,4])))
+xlim <- range(change[,1])
+par(fig = c(.6, 1, 0.6, 1), mar=c(0,0,0,0), new=TRUE)
+plot(change[,1],change[,4],type="l",xlab="Year",ylab="",
+     ylim=c(0,max(change[,4])),bg="grey",add=T,bty="n")
+
+## set wd to get data
+nzers<-read.csv("monthtravel.csv",header=T)
+head(nzers)
+names(nzers)
+dim(nzers)
+
+plot(as.numeric(nzers[247,2:79]),type="l",ylim=c(min(as.numeric(nzers[247,2:79])),max(ttimes$res)),
+)
+points(ttimes$res,add=T,type="l",col="red")
+
+par(mar = c(7, 4, 4, 2) + 0.1)
+plot(as.numeric(nzers[247,8:79]),type="l",xaxt="n",xlab="",ylab="Numbers",
+     ,ylim=c(min(as.numeric(nzers[247,2:79])),max(ttimes$res)))
+points(ttimes$res,type="l",col="red")
+# Set up x axis with tick marks alone
+# Create arbitrary text
+labels <- paste(ttimes$mydate, sep = " ")
+axis(side=1, at=1:71, labels=labels,las=2)
+legend("topleft",c("New Zealanders","Non-New Zealanders"),
+       bty="n",col=c("black","red"),lty=1)
+# check same length
+length(nzers[247,8:78])
+length(ttimes$res)
+
+nz<-nzers[247,8:78]
+im<-ttimes$res
+newd<-rbind(nz,im)
+rownames(newd)<-c("nz","im")
+colnames(newd)<-ttimes$mydate
+sum<-colSums(newd)
+newd<-rbind(sum,newd)
+rownames(newd)<-c("total","nz","im")
+
+pdf(paste("nzers.pdf"), width=5, height=5)
+plot(as.numeric(newd[1,]),type="l",xaxt="n",xlab="",ylab="Numbers",
+     ,ylim=c(min(newd),max(newd)),cex.lab=0.6,cex.axis=0.6)
+points(as.numeric(newd[2,]),type="l",col="red")
+points(as.numeric(newd[3,]),type="l",col="black",lty=2)
+# Set up x axis with tick marks alone
+# Create arbitrary text
+labels <- paste(colnames(newd), sep = " ")
+axis(side=1, at=seq(from=1,to=71,by=2), cex=0.5,labels=labels[seq(from=1,to=71,by=2)],las=2,cex.axis=0.6)
+abline(v=seq(from=6,to=71,by=12),lty=3,col="grey")
+legend("topleft",bg="white",c("Total","New Zealanders","Non-New Zealanders"),
+       col=c("black","red","black"),lty=c(1,1,2),box.col=rgb(0,0,0,alpha=0.5) , cex=0.6)
+dev.off()
+
+plot(as.numeric(newd[1,]),type="l",xaxt="n",xlab="",ylab="Numbers",
+     ,ylim=c(min(newd),max(newd)))
+points(as.numeric(newd[2,]),type="l",col="red")
+points(as.numeric(newd[3,]),type="l",col="black",lty=2)
+# Set up x axis with tick marks alone
+# Create arbitrary text
+labels <- paste(colnames(newd), sep = " ")
+axis(side=1, at=seq(from=1,to=71,by=2), labels=labels[seq(from=1,to=71,by=2)],las=2)
+abline(v=seq(from=6,to=71,by=12),lty=3,col="grey")
+legend("topleft",bg="white",c("Total","New Zealanders","Non-New Zealanders"),
+       col=c("black","red","black"),lty=c(1,1,2),box.col=rgb(0,0,0,alpha=0.5) )
+# check same length
